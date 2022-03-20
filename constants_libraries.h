a9845580525c576f4987fc5c57592a7993da9265
yungtimofey <72038900+yungtimofey@users.noreply.github.com>
2022-03-20 13:34:02 +0300

    Add files via upload

---
+++ constants_libraries.h
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <exception>
+#include <algorithm>
+#include <regex>
+
+#define HEAP_SIZE 128
+#define byte uint8_t
+#define uint unsigned int
+#define HEADER sizeof(uint)
+
+#define sign bool
+#define SIGN_SIZE sizeof(sign)
+
+#define link byte*
+#define LINK_SIZE sizeof(link)
